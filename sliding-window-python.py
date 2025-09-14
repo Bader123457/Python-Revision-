59ae4ff18e2cc174a0fac7fa5d89d6ffc6059542
Bader123457 <ibadr2005@gmail.com>
2025-09-14 18:04:47 +0100

    1/3 of sw vid1

--- sliding-window-python.py
+++ sliding-window-python.py
@@ -1,6 +1,31 @@
 # the sliding window is an algorithm that is mainly used when trying to find sub-arrays or sub-strings 
 # they are very useful and transform time complexity from O(N^2) to O(N) 
 
-# for example let's say we have an array and we want to find the subarray with the maximum sum but it has to be of size k 
-# we start by creating a subarray (window) from index 0 to k-1 and then we slide it by one so the window is now at i+1 to k all we need to do is to subtract i from the sum and add k to the sum 
-  
+# here is a good example 
+# let's imagine we have an array and a value k  
+# what we need to do is find a maximum sum subarray of size k 
+# here we use sliding window 
+
+nums = [8,3,-2,4,5,-1,0,5,3,9.-6] 
+k = 5 
+
+# here we would start from 8 and create a window up to 5 which has size k 
+# then we compute the sum which is 
+print(sum(nums[0:5])) # this subarray or window has k (5) elements inside the ans is 13 
+# then we do nums[1:6] and then nums[2:7] and so on 
+# we compute the sum each time and take the largest one as our answer 
+
+# do this has time complexity of O(N*k) which is slow 
+
+# so instead of doing it this way we follow the sliding window 
+# we do it very similar but instead of computing the sum each time what we do is that we subtract the element removed from the window and sum the element added to the window 
+# here is an example 
+first_sub = [8,3,-2,4,5] # first window 
+second_sub = [3,-2,4,5,-1] # second window 
+# instead of computing the sum on both these windows we just compute the sum of the first window
+# and then we subtract 8 (removed from window) from it and add -1 (new element to our window) 
+# so the new sum is 18 -8 +-1 = 9  
+# remember our subarray(our window) is size k
+
+# to our sum we add the one that entered the window and subtract the one that left 
+# to every window we only perform 1 addition and 1 subtraction 
