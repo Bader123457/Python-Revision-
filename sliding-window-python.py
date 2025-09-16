4bec32c70b9cfd608942f00bf425476ec0952104
Bader123457 <ibadr2005@gmail.com>
2025-09-16 19:49:27 +0100

    question redone 2

--- sliding-window-python.py
+++ sliding-window-python.py
@@ -128,7 +128,31 @@ for R in range(len(nums)):
         L += 1
 
 print(best)  # 6
- 
+
+
+# here is another way to code it 
+
+def maxSum(arr, k):
+    
+    
+    n = len(arr)
+
+
+    # Compute sum of first window of size k
+    window_sum = sum(arr[:k])
+
+    # first sum available
+    max_sum = window_sum
+
+    # Compute the sums of remaining windows by
+    # removing first element of previous
+    # window and adding last element of
+    # the current window.
+    for i in range(n - k):
+        window_sum = window_sum - arr[i] + arr[i + k]
+        max_sum = max(window_sum, max_sum)
+
+    return max_sum
 
 
 
