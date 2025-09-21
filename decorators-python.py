4b84edef3be5b33af865fe4d130f0786fae288d3
Bader123457 <ibadr2005@gmail.com>
2025-09-21 16:22:17 +0100

    decos done

--- decorators-python.py
+++ decorators-python.py
@@ -33,3 +33,41 @@ print_message("I am 19 years of age  ")
 
 # output will be hello I am 19 years of age 
  
+
+# now we are done with all of this 
+# let's create a decorator 
+
+def printer():
+    print("hello world") 
+
+def display_info(func):
+    def inner():
+        print("executing", func.__name__,"function") 
+        func()
+        print("finished") 
+    return inner 
+print(display_info(printer))
+
+
+
+
+# here is a very good example for it 
+
+def my_decorator(func):
+    def wrapper():
+        print("Something happens *before* the function is called.")
+        func()
+        print("Something happens *after* the function is called.")
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("Hello!")
+
+say_hello()
+
+
+# it's syntax comes after @ 
+# so we applied say_hello() to my_decorator()
+
+
