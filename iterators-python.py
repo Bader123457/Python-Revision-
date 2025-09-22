5760560f41fca8c91a49966cf19c6f1fea2de74d
Bader123457 <ibadr2005@gmail.com>
2025-09-22 15:53:25 +0100

    defining iterators

---
+++ iterators-python.py
@@ -0,0 +1,21 @@
+# what are iterators in python ?
+
+# iterators are methods that iterate collections like lists,tuples,etc 
+# They are able to loop through the object and return its elements 
+
+# A python iterator object must implement 2 special methods 
+# 1 - iter()
+# 2 - next() 
+# together they are known as the iterator protocol 
+
+# let's look at examples 
+arr = [4,7,0] 
+
+iterator = iter(arr) # we created an iterator from our list 
+# get the first element from the iterator 
+print(next(iterator)) # 4 
+# second element 
+print(next(iterator)) # 7 
+# third element 
+print(next(iterator)) # 0 
+
