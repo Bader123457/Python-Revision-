a97598e27b8e8b3a7302e0850f0b6695759077f3
Bader123457 <ibadr2005@gmail.com>
2025-09-22 19:26:57 +0100

    defining regex

---
+++ regex-python.py
@@ -0,0 +1,3 @@
+# what are regular expression in python?
+# a sequence of characters that specifies a search pattern in text. Usually such patterns are used by string-searching algorithms for "find" or "find and replace" operations on strings, or for input validation.
+
