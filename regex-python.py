d591b8ca3800e3e35fd65a089b93bb61573ac6e7
Bader123457 <ibadr2005@gmail.com>
2025-09-22 19:37:09 +0100

    importing regex

--- regex-python.py
+++ regex-python.py
@@ -1,3 +1,6 @@
 # what are regular expression in python?
 # a sequence of characters that specifies a search pattern in text. Usually such patterns are used by string-searching algorithms for "find" or "find and replace" operations on strings, or for input validation.
 
+# regular expression are a module in python known as re 
+# to use it:
+import re 
