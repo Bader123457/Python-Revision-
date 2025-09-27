2a6dcc5a0ba664bc0c40e224f008e52f4318e0fb
Bader123457 <ibadr2005@gmail.com>
2025-09-27 23:14:05 +0100

    using all and any for the first time

--- and&all-python.py
+++ and&all-python.py
@@ -1,5 +1,12 @@
-def add(x,y):
-    return x+y 
+# they are 2 major functions in python they are used to check the content of a collection 
 
+# any will return True if any element in the collection is true 
+# all will only return True if one element in the collection is True 
 
-print(add(1,2)) 
+print(any([True,False,False])) # True 
+print(any([False,False,False])) # False
+
+# only 1 value has to be true 
+
+print(all([False,True,True,True])) # False 
+print(all([True,True,True,True])) # True  
